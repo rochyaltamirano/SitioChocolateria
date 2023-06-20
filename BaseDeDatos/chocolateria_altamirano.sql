2888388e49b3375a67543138aaf0174febc612d4
rochyaltamirano <rochyaltamirano@gmail.com>
2023-06-19 23:50:55 -0300

    update base de datos

--- BaseDeDatos/chocolateria_altamirano.sql
+++ BaseDeDatos/chocolateria_altamirano.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: localhost:8889
--- Tiempo de generación: 30-05-2023 a las 18:54:02
+-- Tiempo de generación: 20-06-2023 a las 02:49:06
 -- Versión del servidor: 5.7.39
 -- Versión de PHP: 7.4.33
 
@@ -20,6 +20,71 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `chocolateria_altamirano`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `faqs`
+--
+
+CREATE TABLE `faqs` (
+  `id` int(11) NOT NULL,
+  `pregunta` text NOT NULL,
+  `respuesta` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `faqs`
+--
+
+INSERT INTO `faqs` (`id`, `pregunta`, `respuesta`) VALUES
+(1, '¿Realizan envíos a domicilio?', 'Por el momento no, solo retiro por nuestro local.'),
+(2, '¿Cómo puedo abonar?', 'Aceptamos efectivo, transferencias, todas las tarjetas de crédito, débito y MercadoPago.'),
+(3, '¿Hay descuento si compro en efectivo?', 'Si, tenemos un 10% de descuento si pagas en efectivo.');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `novedades`
+--
+
+CREATE TABLE `novedades` (
+  `id` int(11) NOT NULL,
+  `titulo` varchar(100) NOT NULL,
+  `novedad` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `novedades`
+--
+
+INSERT INTO `novedades` (`id`, `titulo`, `novedad`) VALUES
+(1, '¡Estamos contratando!', 'Si te interesa trabajar con nosotrosm acercanos tu CV a nuestra sucursal.'),
+(2, '¡Nuevo sabor!', '¿Ya probaste nuestro nuevo sabor chocolate triple A? Tiene una mezcla de chocolate, nueces y un ingrediente secreto.'),
+(3, '¡Nueva sucursal!', 'Pronto abriremos una nueva sucursal en el Tortugas Open Mall.'),
+(6, 'lal', 'lalala');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `promociones`
+--
+
+CREATE TABLE `promociones` (
+  `id` int(11) NOT NULL,
+  `titulo` varchar(100) NOT NULL,
+  `descripcion` varchar(250) NOT NULL,
+  `precio` int(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `promociones`
+--
+
+INSERT INTO `promociones` (`id`, `titulo`, `descripcion`, `precio`) VALUES
+(1, 'Promo 1', 'Caja de 8 bombones a elección', 850),
+(3, 'Promo 2', 'Cajas personalizadas', 1500),
+(4, 'Promo 3', 'Caja corazón con 13 bombones', 1600);
 
 -- --------------------------------------------------------
 
@@ -45,6 +110,24 @@ INSERT INTO `usuarios` (`id`, `usuario`, `password`) VALUES
 --
 
 --
+-- Indices de la tabla `faqs`
+--
+ALTER TABLE `faqs`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `novedades`
+--
+ALTER TABLE `novedades`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `promociones`
+--
+ALTER TABLE `promociones`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indices de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
@@ -53,6 +136,24 @@ ALTER TABLE `usuarios`
 --
 -- AUTO_INCREMENT de las tablas volcadas
 --
+
+--
+-- AUTO_INCREMENT de la tabla `faqs`
+--
+ALTER TABLE `faqs`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `novedades`
+--
+ALTER TABLE `novedades`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT de la tabla `promociones`
+--
+ALTER TABLE `promociones`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT de la tabla `usuarios`
